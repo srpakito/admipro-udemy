79b4c488f0d27cb92850dded8f487b35794ac01b
Juan Pablo Vargas 'MrPakito <mrpakito@icloud.com>
2019-10-02 18:39:05 -0500

    Primer Commit

---
+++ src/app/pages/graficas01/graficas01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas01',
+  templateUrl: './graficas01.component.html',
+  styles: []
+})
+export class Graficas01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
